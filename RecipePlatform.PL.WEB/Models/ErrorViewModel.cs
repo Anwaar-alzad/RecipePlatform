be4fee01009e2a7e3f50ccd5da4f15147133b8cd
Anwaar-alzad <anwaarmalzadjali@gmail.com>
2025-06-22 09:16:09 +0400

    Ignore .vs folder

---
+++ RecipePlatform.PL.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecipePlatform.PL.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
